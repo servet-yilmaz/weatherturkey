cf68e7e787c681bb3077602c00ff82e419a4dfaf
Servet Yılmaz <42248811+servet-yilmaz@users.noreply.github.com>
2024-09-15 11:49:49 +0300

    Add files via upload

---
+++ static/js/952.8391025e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdaki=self.webpackChunkdaki||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.8391025e.chunk.js.map
